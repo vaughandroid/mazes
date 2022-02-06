22caec15502c72fd6261add75400802866349351
Chris Vaughan <vaughandroid@gmail.com>
2022-02-06 16:45:28 +0000

    Create and render an empty grid.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import logUpdate from 'log-update';
 
+const width = 20;
+const height = 10;
 const frames = ['-', '\\', '|', '/'];
-let index = 0;
+const grid = createEmptyGrid(width, height);
 
-setInterval(() => {
-  const frame = frames[index = ++index % frames.length];
+function createEmptyGrid(width, height) {
+  const grid = new Array(height);
+  for (let rowIdx = 0; rowIdx < height; rowIdx++) {
+    grid[rowIdx] = new Array(width).fill(' ');
+  }
+  return grid;
+}
 
-  logUpdate(`${frame} mazes are amazing ${frame}`);
-}, 80);
+function renderGrid(grid) {
+  let output = '';
+
+  function addTopBottomBorder() {
+    output = output + '+';
+    for (let i = 0; i < width; i++) {
+      output = output + '-';
+    }
+    output = output + '+\n';
+  }
+
+  addTopBottomBorder();
+
+  grid.forEach(row => {
+    output = output + '|';
+    row.forEach(cell => { output = output + cell; })
+    output = output + '|\n';
+  });
+
+  addTopBottomBorder();
+
+  return output;
+}
+
+function main() {
+  let index = 0;
+  setInterval(() => {
+    const frame = frames[index = ++index % frames.length];
+    logUpdate(`${frame} mazes are amazing ${frame}\n${renderGrid(grid)}`);
+  }, 80);
+}
+
+main();
