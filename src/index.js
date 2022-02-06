eaa843b8a10d268e41d557f9166bd89f205c5b8b
Chris Vaughan <vaughandroid@gmail.com>
2022-02-06 16:15:48 +0000

    Add log-update for rendering to the console.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, mazes!');
+import logUpdate from 'log-update';
+
+const frames = ['-', '\\', '|', '/'];
+let index = 0;
+
+setInterval(() => {
+  const frame = frames[index = ++index % frames.length];
+
+  logUpdate(`${frame} mazes are amazing ${frame}`);
+}, 80);
