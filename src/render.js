b322ceb0bdf8478177bbfe7c3722d517b141427c
Chris Vaughan <Chris.Vaughan01@bbc.co.uk>
2022-03-01 15:35:10 +0000

    Tweak console rendering.

--- src/render.js
+++ src/render.js
@@ -3,43 +3,25 @@ import logUpdate from "log-update";
 export function renderGrid(grid) {
   let output = '';
 
-  // Top border
-  output = output + '+';
-  for (let i = 0; i < grid.width; i++) {
-    output = output + '--';
-  }
-  output = output + '-+\n';
-
-  // Cells & side borders
+  // Cells
   grid.rows.forEach(row => {
-    // Left border
-    output = output + '|';
+    row.forEach(cell => {
+      output = output + `+${cell.walls.top ? '—' : ' '}`;
+    })
+    output = output + '+\n';
 
     row.forEach(cell => {
-      output = output + `#${cell.walls.top ? '#' : ' '}`;
+      output = output + `${cell.walls.left ? '|' : ' '} `;
     })
-    output = output + '#|\n';
-
-    output = output + '|';
-    row.forEach(cell => {
-      output = output + `${cell.walls.left ? '#' : ' '} `;
-    })
-    output = output + '#|\n';
+    output = output + '|\n';
   });
 
   // Bottom walls
-  output = output + '|';
-  for (let i = 0; i < grid.width; i++) {
-    output = output + '##';
-  }
-  output = output + '#|\n';
-
-  // Bottom border
   output = output + '+';
   for (let i = 0; i < grid.width; i++) {
-    output = output + '--';
+    output = output + '——';
   }
-  output = output + '-+\n';
+  output = output + '—+\n';
 
   logUpdate(output);
 }
